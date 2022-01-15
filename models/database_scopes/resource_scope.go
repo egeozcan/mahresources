00a04755b9e6f67638f24a779991278e949834a6
Ege Özcan <egecan@gmail.com>
2022-01-15 14:17:52 +0100

    don't treat owner groups like related too

--- models/database_scopes/resource_scope.go
+++ models/database_scopes/resource_scope.go
@@ -45,7 +45,7 @@ func ResourceQuery(query *query_models.ResourceSearchQuery, ignoreSort bool, ori
 					FROM groups_related_resources grr 
 					WHERE grr.resource_id = resources.id AND grr.group_id IN ? AND grr.group_id <> resources.owner_id)
 				= ? 
-			`, query.Groups, query.Groups, len(query.Groups))
+			`, query.Groups, len(query.Groups))
 		}
 
 		if query.Notes != nil && len(query.Notes) > 0 {
